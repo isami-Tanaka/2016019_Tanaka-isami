327f2371c4ebd87309e4a40326bef4721dd7adb3
isami-Tanaka <2016019@s.asojuku.s.ac.jp>
2021-08-06 16:56:50 +0900

    ad

--- shootemup/shootemup/Bullet.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Geometry.h"
-
-struct Bullet {
-	Position2 pos;//À•W
-	Vector2 vel;//‘¬“x
-	bool isActive = false;//¶‚«‚Ä‚é‚©`H
-};
--- shootemup/shootemup/HomingShot.cpp
+++
@@ -1,6 +0,0 @@
-#include "HomingShot.h"
-
-HomingShot::HomingShot() :trail(*this)	//this:自分自身のポインター
-{
-
-}
